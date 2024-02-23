ae00236e16eaf575468481720c4b23dc0e12d212
Rion Dsilva <118284932+RionDsilvaCS@users.noreply.github.com>
2024-02-23 15:41:27 +0530

    remove config

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from config import app
+from flask import Flask
+from flask_cors import CORS
 from google_playstore.google_playstore import google_playstore_blueprint
 from app_store.app_store import app_store_blueprint
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(google_playstore_blueprint, url_prefix='/google-playstore')
 app.register_blueprint(app_store_blueprint, url_prefix='/app-store')
 
-app.run()
+# app.run()
